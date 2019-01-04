d65dc74679895823c1d44ab847bab90a51c83777
Brady Bentley <bradybentley@Bradys-MacBook-Pro.local>
2019-01-04 11:28:43 -0700

    first commit

---
+++ Assessment5/Model/Contact.swift
@@ -0,0 +1,9 @@
+//
+//  Contact.swift
+//  Assessment5
+//
+//  Created by Brady Bentley on 1/4/19.
+//  Copyright © 2019 Brady. All rights reserved.
+//
+
+import Foundation
---
+++ Assessment5/ViewControllers/ContactDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactDetailViewController.swift
+//  Assessment5
+//
+//  Created by Brady Bentley on 1/4/19.
+//  Copyright © 2019 Brady. All rights reserved.
+//
+
+import UIKit
+
+class ContactDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
