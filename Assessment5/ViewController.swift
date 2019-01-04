46cc9dc60afe07204279b484870f22b06122b238
Brady Bentley <bradybentley@bradys-macbook-pro.local>
2019-01-04 09:05:25 -0700

    Initial Commit

---
+++ Assessment5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assessment5
+//
+//  Created by Brady Bentley on 1/4/19.
+//  Copyright © 2019 Brady. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
